44e42ff8943ef36c2830383cc200d680ccb12696
Juan Meyer <107147413+juanmeyer94@users.noreply.github.com>
2023-12-18 12:26:00 -0300

    Https, test api.

--- CasaTomas/Api/src/app.js
+++ CasaTomas/Api/src/app.js
@@ -8,6 +8,7 @@ import itemsRoutes from "./Routes/items.routes.js"
 import cloudinaryRoutes from "./Routes/cloudinary.routes.js";
 import orderRouter from "./Routes/order.routes.js";
 import dotenv from "dotenv"
+import https from "https"
 
 dotenv.config();
 
@@ -21,8 +22,32 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", cors(), (req, res) => {
-    res.json("Hello");
+app.get("/test-api", (req, res) => {
+    const url = "https://rickandmortyapi.com/api/character/164";
+
+    https.get(url, (response) => {
+        let data = '';
+
+        // Recibir los datos en trozos
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // Cuando se reciben todos los datos
+        response.on('end', () => {
+            try {
+                const jsonData = JSON.parse(data);
+                console.log("Response from Rick and Morty API:", jsonData);
+                res.json({ success: true, message: "Check the console for API response." });
+            } catch (error) {
+                console.error("Error parsing JSON:", error.message);
+                res.status(500).json({ success: false, message: "Error parsing JSON from Rick and Morty API." });
+            }
+        });
+    }).on('error', (error) => {
+        console.error("Error fetching data from Rick and Morty API:", error.message);
+        res.status(500).json({ success: false, message: "Error fetching data from Rick and Morty API." });
+    });
 });
 
 app.use("/api",router);
