b7305e3d2860d49789186372eadd7c8eeda43086
Juan Meyer <107147413+juanmeyer94@users.noreply.github.com>
2023-12-28 10:08:55 -0300

    Update Card.jsx

--- CasaTomas/CasaTomas/src/Components/Card/Card.jsx
+++ CasaTomas/CasaTomas/src/Components/Card/Card.jsx
@@ -1,21 +1,18 @@
-import { useState } from "react";
-import { getItemById } from "../../redux/actions";
-
-const Card = ({ photo, price, name, description, summary, specsTecs, id, marca, openFullCard }) => {
+const Card = ({ photo, price, name,  summary, id, marca, openFullCard }) => {
   return (
-    <div className="max-w-[280px] min-h-[450px] max-h-[450px] bg-sky-200 px-4 pt-2 pb-1 rounded-xl shadow-lg transform hover:scale-105 transition duration-500 my-4 border-dashed relative">
-      <h1 className="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">{marca}</h1>
+    <div className="max-w-[280px] 2xl:min-h-[450px] xl:max-h-[300px] xl:min-h-[300px] 2xl:max-h-[450px] bg-sky-200 px-4 pt-2 pb-1 rounded-xl shadow-lg transform hover:scale-105 transition duration-500 my-4 border-dashed relative">
+      <h1 className="mt-4 text-gray-800 2xl:text-2xl font-bold cursor-pointer text-md">{marca}</h1>
 
       <div className="relative">
-        <img className="rounded-xl" src={photo} alt="Colors" />
-        <p className="absolute top-0 bg-yellow-300 text-gray-800 font-extrabold py-1 px-3 rounded-br-lg rounded-tl-lg ">${price}</p>
+        <img className="rounded-xl xl:h-[100px] xl:w-[130px]" src={photo} alt="Colors" />
+        <p className="absolute top-0 bg-yellow-300 text-gray-800 font-bold py-1 px-3 rounded-br-lg rounded-tl-lg text-xs">${price}</p>
       </div>
 
-      <h3 className="mb-1 text-xl font-bold text-indigo-600 left-0">{name}</h3>
+      <h3 className="mb-1 2xl:text-xl text-sm font-bold text-indigo-600 left-0">{name}</h3>
       <div className="my-1 text-gray-800 font-semibold text-center">
-        <p>{summary}</p>
+        <p className="2xl:text-xl text-xs">{summary}</p>
         <div className="absolute bottom-2 left-0 w-full">
-          <button className="text-xl w-[80%] text-white bg-indigo-600 py-2 rounded-xl shadow-lg" onClick={() => openFullCard(id)}>
+          <button className="text-xl xl:text-sm w-[80%] text-white bg-indigo-600 py-2 rounded-xl shadow-lg" onClick={() => openFullCard(id)}>
             Más información
           </button>
         </div>
