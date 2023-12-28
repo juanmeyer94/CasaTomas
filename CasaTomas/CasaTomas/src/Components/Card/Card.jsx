47de732275731af7b71cad2b50b94b8c4c33e296
Juan Meyer <107147413+juanmeyer94@users.noreply.github.com>
2023-12-28 10:36:54 -0300

    Update Card.jsx

--- CasaTomas/CasaTomas/src/Components/Card/Card.jsx
+++ CasaTomas/CasaTomas/src/Components/Card/Card.jsx
@@ -1,10 +1,11 @@
 const Card = ({ photo, price, name,  summary, id, marca, openFullCard }) => {
   return (
-    <div className="max-w-[280px] 2xl:min-h-[450px] xl:max-h-[300px] xl:min-h-[300px] 2xl:max-h-[450px] bg-sky-200 px-4 pt-2 pb-1 rounded-xl shadow-lg transform hover:scale-105 transition duration-500 my-4 border-dashed relative">
+    <div className="max-w-[280px] 2xl:min-h-[450px] xl:max-h-[300px] xl:min-h-[300px] 2xl:max-h-[450px] xs:max-h-[280px] xs:min-h-[280px] bg-sky-200 px-4 pt-2 pb-1 rounded-xl shadow-lg transform hover:scale-105 transition duration-500 my-4 border-dashed relative">
       <h1 className="mt-4 text-gray-800 2xl:text-2xl font-bold cursor-pointer text-md">{marca}</h1>
 
       <div className="relative">
-        <img className="rounded-xl xl:h-[100px] xl:w-[130px]" src={photo} alt="Colors" />
+        <img className="rounded-xl xl:h-[100px] xl:w-[130px] 2xl:h-[200px] 2xl:w-[220px]
+        xs:h-[80px] xs:w-[110px] " src={photo} alt="Colors" />
         <p className="absolute top-0 bg-yellow-300 text-gray-800 font-bold py-1 px-3 rounded-br-lg rounded-tl-lg text-xs">${price}</p>
       </div>
 
