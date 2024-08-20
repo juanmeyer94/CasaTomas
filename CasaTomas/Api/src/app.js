39cbe988b5fee217baa6e57c036b5b3c7f5ad3cd
juanmeyer94 <juan_meyer@hotmail.com.ar>
2024-08-20 09:43:51 -0300

    origin dev

--- CasaTomas/Api/src/app.js
+++ CasaTomas/Api/src/app.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: ["https://casatomas.onrender.com", "https://casa-tomas-front.vercel.app"],
+    origin: ["http://http://localhost:5173", "https://casa-tomas-front.vercel.app"],
     credentials: true
 }));
 
