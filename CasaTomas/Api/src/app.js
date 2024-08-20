344b8e0f179af3b1ddab4046b67f4dc4014d943e
Juan Meyer <107147413+juanmeyer94@users.noreply.github.com>
2024-08-20 10:03:54 -0300

    Update app.js

--- CasaTomas/Api/src/app.js
+++ CasaTomas/Api/src/app.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: ["http://http://localhost:5173", "https://casa-tomas-front.vercel.app"],
+    origin: ["http://localhost:5173", "https://casa-tomas-front.vercel.app"],
     credentials: true
 }));
 
