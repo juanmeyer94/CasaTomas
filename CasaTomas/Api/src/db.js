dc00b0e46a7e07464ecb2a3975cc0097a50f97ec
juanmeyer94 <juan_meyer@hotmail.com.ar>
2024-08-24 11:42:14 -0300

    new orders schema and counter

--- CasaTomas/Api/src/db.js
+++ CasaTomas/Api/src/db.js
@@ -1,18 +1,20 @@
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import Counter from "./Models/counter.models.js";
 
 dotenv.config();
 
 export const connectDB = async () => {
+    const URL = process.env.VITE_MONGO_URI;
 
-    
+    try {
+        await mongoose.connect(URL);
+        await initializeCounters();
+    } catch (error) {
+        console.error(error);
+    }
+};
 
-    const URL = process.env.VITE_MONGO_URI
-
-    try{
-    await mongoose.connect(URL)
-    console.log("DB is connected")
-}catch(error) {
-    console.log(error)
-}
+const initializeCounters = async () => {
+    await Counter.findByIdAndUpdate("orderNumber", { sequence_value: 0 }, { upsert: true });
 };
