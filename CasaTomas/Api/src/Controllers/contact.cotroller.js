1147ef3c827628089abe066f074d44a2116534ea
juanmeyer94 <juan_meyer@hotmail.com.ar>
2025-04-11 10:23:57 -0300

    get contact messages routes

--- CasaTomas/Api/src/Controllers/contact.cotroller.js
+++ CasaTomas/Api/src/Controllers/contact.cotroller.js
@@ -13,3 +13,13 @@ export const sendContactInfo = async ( req, res) => {
         
     }
 };
+
+export const getContactInfo = async (req, res) => {
+    try {
+        const contactInfo = await contactModels.find();
+        res.status(200).json(contactInfo);
+    } catch (error) {
+        console.error("Error al obtener la orden:", error)
+        res.status(500).json({ message: error.message });
+    }
+}
--- CasaTomas/Api/src/Routes/contact.routes.js
+++ CasaTomas/Api/src/Routes/contact.routes.js
@@ -4,12 +4,14 @@ import {Router} from "express";
 
 import { contactSchema } from "../schemas/contact.schema.js";
 import { validateSchema } from "../middleware/validateSchema.js";
-import { sendContactInfo } from "../Controllers/contact.cotroller.js";
+import { sendContactInfo, getContactInfo } from "../Controllers/contact.cotroller.js";
 
 
 const contactRouter = Router();
 
 
 contactRouter.post("/sendContact", validateSchema(contactSchema),sendContactInfo);
+contactRouter.get("/getContact", getContactInfo);
+
 
 export default contactRouter;
